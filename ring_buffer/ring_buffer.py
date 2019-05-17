8133ad8dad6b809abad984b08b5fe3ce896c0909
JDB0rg <filthyfeather@gmail.com>
2019-05-17 11:47:51 -0600

    Ring buffer works on python tutor but not in VSCode.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,12 +2,21 @@ class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
     self.current = 0
-    self.storage = [None]*capacity
+    self.storage = [None]#*capacity
+
+  class __Full:
+    def append(self, item):
+      self.storage[self.current] = item
+      self.current = (self.current + 1) % self.capacity
+
+    def get(self):
+      return self.storage[self.current:] + self.data[:self.current]
   
   def append(self, item):
     self.storage.append(item)
     if len(self.storage) == self.capacity:
       self.current = 0
+      self.__class__ = self.__Full
   
   def get(self):
       return self.storage
